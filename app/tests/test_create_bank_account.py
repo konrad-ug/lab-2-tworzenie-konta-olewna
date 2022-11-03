9f4117397c09b62ec15481a77029f8a7f1f9eb88
Oskar Lewna <dajcie.suba2002@gmail.com>
2022-11-03 16:49:48 +0100

    100%

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -17,7 +17,7 @@ class TestCreateBankAccount(unittest.TestCase):
         self.assertEqual(pierwsze_konto.pesel,pesel, "PESEL nie został zapisany!") #feature3
         self.assertEqual(len(pierwsze_konto.pesel),11,"Niepoprawny PESEL") #feature4
 
-    def test_tworzenie_konta_kod(self):
+    def test_tworzenie_konta_kod_rok2000(self):
         pesel = "02290206852"
         pierwsze_konto = Konto(self.imie,self.nazwisko,pesel,self.kod)
         self.assertEqual(pierwsze_konto.imie, self.imie, "Imie nie zostało zapisane!")
@@ -29,17 +29,25 @@ class TestCreateBankAccount(unittest.TestCase):
         self.assertEqual(pierwsze_konto.rok_urodzenia>1960,True,"Osoba urodzona po 1960r.")
         self.assertEqual(pierwsze_konto.saldo,50,"Kod promocyjny nie działa!")
 
-    def test_tworzenie_konta_kod_seniorzy(self):
+    def test_tworzenie_konta_kod_rok1800(self):
         pesel = "59823856471"
         pierwsze_konto = Konto(self.imie,self.nazwisko,pesel,self.kod)
-        self.assertEqual(pierwsze_konto.imie, self.imie, "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, self.nazwisko, "Nazwisko nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.pesel,pesel, "PESEL nie został zapisany!") #feature3
-        self.assertEqual(len(pierwsze_konto.pesel),11,"Niepoprawny PESEL") #feature4
-        self.assertEqual(pierwsze_konto.kod_rabatowy[:5],"PROM_","Niepoprawny kod promocyjny")
-        self.assertEqual(len(pierwsze_konto.kod_rabatowy),8,"Niepoprawny kod promocyjny")
         self.assertEqual(pierwsze_konto.rok_urodzenia<=1960,True,"Osoba urodzona po 1960r.")
-        self.assertEqual(pierwsze_konto.saldo,0,"Saldo nie jest zerowe!")
+
+    def test_tworzenie_konta_kod_rok1990(self):
+        pesel = "02090206852"
+        pierwsze_konto = Konto(self.imie,self.nazwisko,pesel,self.kod)
+        self.assertEqual(pierwsze_konto.rok_urodzenia>1960,False)
+
+    def test_tworzenie_konta_kod_rok2100(self):
+        pesel = "02590206852"
+        pierwsze_konto = Konto(self.imie,self.nazwisko,pesel,self.kod)
+        self.assertEqual(pierwsze_konto.rok_urodzenia>1960,True,"Osoba urodzona po 1960r.")
+
+    def test_tworzenie_konta_kod_rok2200(self):
+        pesel = "02790206852"
+        pierwsze_konto = Konto(self.imie,self.nazwisko,pesel,self.kod)
+        self.assertEqual(pierwsze_konto.rok_urodzenia>1960,True,"Osoba urodzona po 1960r.")
 
     def test_udany_przelew_wychodzacy(self):
         pesel = "01234567890"
