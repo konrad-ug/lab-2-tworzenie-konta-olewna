f53b263fb5c15e411ab798d332de7d097f308048
Oskar Lewna <dajcie.suba2002@gmail.com>
2022-11-09 23:00:30 +0100

    homework

--- app/tests/test_history.py
+++ app/tests/test_history.py
@@ -8,13 +8,29 @@ class TestCreateBankAccount(unittest.TestCase):
     nazwisko = "Januszewski"
     pesel = "01234567890"
     nazwa_firmy = "Zabka"
+    nip = "0123456789"
 
     def test_udany_zapisywanie_przelewu_przychodzacego(self):
         konto = Konto(self.imie,self.nazwisko,self.pesel)
         konto.zaksieguj_przelew_przychodzacy(100)
         self.assertListEqual(konto.historia,[100])
 
-    def test_udane_zapisywanie_przelewu_wychodzacego(self):
-        konto = Konto(self.imie,self.nazwisko,self.pesel)
+    def test_udany_zapisywanie_przelewu_wychodzacego(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel)
         konto.zaksieguj_przelew_przychodzacy(100)
-        self.assertListEqual(konto.historia,[-100])
+        konto.zaksieguj_przelew_wychodzacy(100)
+        self.assertListEqual(konto.historia,[100,-100])
+
+    def test_udany_zapisywanie_przelewu_ekspresowego_osoba(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel)
+        konto.zaksieguj_przelew_przychodzacy(100)
+        konto.przelew_ekspresowy(100)
+        self.assertListEqual(konto.historia,[100,-100,-1])
+
+    def test_udany_zapisywanie_przelewu_ekspresowego_firma(self):
+        konto_firma = Konto_firmowe(self.nazwa_firmy,self.nip)
+        konto_firma.zaksieguj_przelew_przychodzacy(100)
+        konto_firma.przelew_ekspresowy(100)
+        self.assertListEqual(konto_firma.historia,[100,-100,-5])
+
+    
