db0f5d0387f70a398d7670556f18dfb4cf5d7ca5
Oskar Lewna <dajcie.suba2002@gmail.com>
2022-11-10 17:42:37 +0100

    setUp

--- app/tests/test_history.py
+++ app/tests/test_history.py
@@ -10,27 +10,27 @@ class TestCreateBankAccount(unittest.TestCase):
     nazwa_firmy = "Zabka"
     nip = "0123456789"
 
+    def setUp(self):
+        self.konto = Konto(self.imie,self.nazwisko,self.pesel)
+
     def test_udany_zapisywanie_przelewu_przychodzacego(self):
-        konto = Konto(self.imie,self.nazwisko,self.pesel)
-        konto.zaksieguj_przelew_przychodzacy(100)
-        self.assertListEqual(konto.historia,[100])
+        self.konto.zaksieguj_przelew_przychodzacy(100)
+        self.assertListEqual(self.konto.historia,[100])
 
     def test_udany_zapisywanie_przelewu_wychodzacego(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.zaksieguj_przelew_przychodzacy(100)
-        konto.zaksieguj_przelew_wychodzacy(100)
-        self.assertListEqual(konto.historia,[100,-100])
+        self.konto.zaksieguj_przelew_przychodzacy(100)
+        self.konto.zaksieguj_przelew_wychodzacy(100)
+        self.assertListEqual(self.konto.historia,[100,-100])
 
     def test_udany_zapisywanie_przelewu_ekspresowego_osoba(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.zaksieguj_przelew_przychodzacy(100)
-        konto.przelew_ekspresowy(100)
-        self.assertListEqual(konto.historia,[100,-100,-1])
+        self.konto.zaksieguj_przelew_przychodzacy(100)
+        self.konto.przelew_ekspresowy(100)
+        self.assertListEqual(self.konto.historia,[100,-100,-1])
 
     def test_udany_zapisywanie_przelewu_ekspresowego_firma(self):
-        konto_firma = Konto_firmowe(self.nazwa_firmy,self.nip)
-        konto_firma.zaksieguj_przelew_przychodzacy(100)
-        konto_firma.przelew_ekspresowy(100)
-        self.assertListEqual(konto_firma.historia,[100,-100,-5])
+        self.konto_firma = self.konto_firmowe(self.nazwa_firmy,self.nip)
+        self.konto_firma.zaksieguj_przelew_przychodzacy(100)
+        self.konto_firma.przelew_ekspresowy(100)
+        self.assertListEqual(self.konto_firma.historia,[100,-100,-5])
 
     
