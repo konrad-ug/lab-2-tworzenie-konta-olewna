a2a6d6f680d6d3bad757f97d92377cff64fcee66
Oskar Lewna <dajcie.suba2002@gmail.com>
2022-11-24 19:02:48 +0100

    rejestry + flask

---
+++ app/RejestrKont.py
@@ -0,0 +1,19 @@
+from app.Konto import Konto
+
+class RejestrKont():
+    lista_kont = []
+
+    @classmethod
+    def dodaj_konto(cls,konto: Konto):
+        cls.lista_kont.append(konto)
+
+    @classmethod
+    def ile_kont(cls):
+        return len(cls.lista_kont)
+
+    @classmethod
+    def wyszukaj_konto_po_peselu(cls,pesel):
+        for k in cls.lista_kont:
+            if k.pesel == pesel:
+                return k
+        return None
