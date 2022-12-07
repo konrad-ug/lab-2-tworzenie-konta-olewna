5c8076779a3113eaaf13f3592fb09d37ef7988b2
Oskar Lewna <dajcie.suba2002@gmail.com>
2022-12-07 19:29:51 +0100

    feature 16 - poprawka

--- app/tests/test_rejestry.py
+++ app/tests/test_rejestry.py
@@ -8,6 +8,10 @@ class TestRejestry(unittest.TestCase):
     nazwisko = "Januszewski"
     pesel = "01234567890"
     pesel2 = "11111111111"
+    dane = {
+        "imie": "Bartek",
+        "saldo":17
+    }
 
     @classmethod
     def setUpClass(cls):
@@ -39,12 +43,29 @@ class TestRejestry(unittest.TestCase):
         konto = Konto(self.imie,self.nazwisko,"09876543211")
         dodaj_konto = RejestrKont.dodaj_konto(konto)
         self.assertEqual(dodaj_konto, konto)
-        konto_do_aktualizacji = RejestrKont.zaaktualizuj_konto_po_peselu("09876543211",{"imie":"Bartek","saldo":17})
-        self.assertEqual(konto_do_aktualizacji,{"imie":"Bartek","nazwisko":"Januszewski","pesel":"09876543211","saldo":17})
+        konto_do_aktualizacji = RejestrKont.zaaktualizuj_konto_po_peselu("09876543211",self.dane)
+        self.assertEqual(konto_do_aktualizacji.imie, self.dane['imie'])
+        self.assertEqual(konto_do_aktualizacji.nazwisko, self.nazwisko)
+        self.assertEqual(konto_do_aktualizacji.saldo, self.dane['saldo'])
+        # self.assertEqual(konto_do_aktualizacji,{"imie":"Bartek","nazwisko":"Januszewski","pesel":"09876543211","saldo":17})
+
+    def test_nieudane_dodanie_konta(self):
+        konto = Konto(self.imie,self.nazwisko,self.pesel)
+        dodaj_konto = RejestrKont.dodaj_konto(konto)
+        self.assertEqual(dodaj_konto, None)
 
     def test_usuwanie_konta_po_peselu(self):
+        konto = Konto(self.imie,self.nazwisko,"09876543211")
+        RejestrKont.dodaj_konto(konto)
         ile_kont = RejestrKont.ile_kont()
         usunięte_konto = RejestrKont.usun_konto_po_peselu('09876543211')
+        self.assertEqual(usunięte_konto, konto)
         bez_usuniętego = RejestrKont.ile_kont()
-        self.assertEqual(usunięte_konto, None)
+        self.assertEqual(ile_kont-1,bez_usuniętego)
+
+    def test_nieudane_usuwanie_konta_po_peselu(self):
+        ile_kont = RejestrKont.ile_kont()
+        usunięte_konto = RejestrKont.usun_konto_po_peselu('98989898989')
+        self.assertEqual(usunięte_konto,None)
+        bez_usuniętego = RejestrKont.ile_kont()
         self.assertEqual(ile_kont,bez_usuniętego)
